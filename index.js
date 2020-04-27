c12847555c351f5cb503e0377e8fe2861b5f3fb6
morrisseyjsean <morrisseyjsean@gmail.com>
2020-04-27 10:17:29 +0800

    Exercise 3.7:  Implemented morgan

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 let persons = [
@@ -20,6 +21,7 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
