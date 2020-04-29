b3e1c9000c4071e412a16959738d2e9e9e13186e
morrisseyjsean <morrisseyjsean@gmail.com>
2020-04-29 11:00:17 +0800

    Exercise 3.14: Added database delete functionality

--- index.js
+++ index.js
@@ -101,9 +101,15 @@ app.post("/api/persons", (req, res) => {
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id != id);
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // const id = Number(req.params.id);
+  //persons = persons.filter((person) => person.id != id);
 });
 
 const PORT = process.env.PORT || 3001;
